64628f6775381a7e6a54261890a1bf745ac5e27a
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-23 23:44:32 +0300

    js continue

---
+++ javaScript.js
@@ -0,0 +1,6 @@
+function hesapla() {
+
+   let kilo = parseFloat(document.getElementById("input1").value);
+   let boy = parseFloat(document.getElementById("input2").value);
+
+   let vki = kilo/boy**2;
